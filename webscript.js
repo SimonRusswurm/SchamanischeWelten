83543d8a13508179180a86c0d7aa134109c06c70
SimonRusswurm <70999157+SimonRusswurm@users.noreply.github.com>
2020-09-21 17:49:55 +0200

    Update

--- webscript.js
+++ webscript.js
@@ -5,3 +5,14 @@ const links = document.querySelectorAll(".nav-links li");
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-60px";
+  }
+  prevScrollpos = currentScrollPos;
+}
