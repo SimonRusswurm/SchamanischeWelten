d2fe60e4f1a2da3c7c780278464412e25a7ea536
Simon Russwurm <simon.russwurm@gmail.com>
2020-11-15 19:20:50 +0100

    update

--- webscript.js
+++ webscript.js
@@ -6,13 +6,42 @@ hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 });
 
+/* **************************** NAV ON SCROLL **************************** */
+
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+  if(currentScrollPos < 0){
+	  currentScrollPos = 0;
+  }
+  if (prevScrollpos >= currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-60px";
   }
   prevScrollpos = currentScrollPos;
 }
+
+class info{
+	constructor(dateString, locationString, maxPersons){
+		this.date = dateString;
+		this.location = locationString;
+		this.maxPersons = maxPersons;	
+	}
+}
+
+class seminar{
+	constructor(seminartitel, description, infoClass, pdfName){
+		this.title = seminartitel;
+		this.description = description;
+		this.info = infoClass;
+		this.pdf = pdfName;
+	}
+}
+
+const createNewSeminar = (seminar) => {
+	document.createElement('div')
+	
+	
+}
+
